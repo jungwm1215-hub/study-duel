b048ad61df59692d0921c9964a73bbf61072f9fe
JUNGWOOMIN <GitHubjungwm1215@gmail.com>
2026-06-01 20:15:28 +0900

    fix oauth timing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { supabase } from './supabase';
+
+// OAuth hash 토큰을 앱 마운트 전에 먼저 처리
+const hash = window.location.hash;
+if(hash.includes('access_token')){
+  const params = new URLSearchParams(hash.substring(1));
+  const access_token = params.get('access_token');
+  const refresh_token = params.get('refresh_token');
+  if(access_token && refresh_token){
+    supabase.auth.setSession({ access_token, refresh_token }).then(()=>{
+      window.history.replaceState(null, '', window.location.pathname);
+    });
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +24,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
