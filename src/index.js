98defe93b2aa7c217abd946b6ea00a9406325248
JUNGWOOMIN <GitHubjungwm1215@gmail.com>
2026-06-01 20:40:10 +0900

    revert index

--- src/index.js
+++ src/index.js
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { supabase } from './supabase';
-
-const hash = window.location.hash;
-if(hash.includes('access_token')){
-  const params = new URLSearchParams(hash.substring(1));
-  const access_token = params.get('access_token');
-  const refresh_token = params.get('refresh_token');
-  if(access_token && refresh_token){
-    window.history.replaceState(null, '', window.location.pathname);
-    supabase.auth.setSession({ access_token, refresh_token })
-      .then(({data, error})=>{
-        console.log('setSession result:', JSON.stringify({data: !!data?.session, error}));
-      });
-  }
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
